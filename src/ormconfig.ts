71d17e7119e322df6ad5b538ac86499565c84fd9
Viktoriia <nazaruk.v92@gmail.com>
2023-08-25 21:07:04 +0300

    Update db config

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,25 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
+// const config: PostgresConnectionOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'q1w2e3r4',
+//   database: 'kapusta',
+//   synchronize: false,
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+// };
+
+// export default config;
 const config: PostgresConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'postgres://kapustapsqluser:FrDOffMVTjZm19EFtMA602eALq70z7mR@dpg-cjjp5f8cfp5c738ikr70-a/kapustapsqldb',
   port: 5432,
-  username: 'postgres',
-  password: 'q1w2e3r4',
-  database: 'kapusta',
+  username: 'kapustapsqldb',
+  password: 'FrDOffMVTjZm19EFtMA602eALq70z7mR',
+  database: 'kapustapsqldb',
   synchronize: false,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
